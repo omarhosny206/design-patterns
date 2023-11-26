86bb9f66149e4d755cf56576d5dd5a9a9e69be1a
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-11-26 23:40:09 +0200

    Update Main.java

--- src/creational/factory/simplefactory/Main.java
+++ src/creational/factory/simplefactory/Main.java
@@ -3,7 +3,7 @@ package creational.factory.simplefactory;
 public class Main {
     public static void main(String[] args) {
         PayloadParser jsonPayloadParser = PayloadParserFactory.getPayloadParser("JSON");
-        PayloadParser xmlPayloadParser = PayloadParserFactory.getPayloadParser("ML");
+        PayloadParser xmlPayloadParser = PayloadParserFactory.getPayloadParser("XML");
         jsonPayloadParser.parse();
         xmlPayloadParser.parse();
     }
